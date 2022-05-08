21a2bc2a909fd8fbaf0e41b73de0ca1a9ab287df
Deepakparashari <parasharivip@gmail.com>
2022-05-08 15:09:04 +0530

    test
    Test

--- Track.js
+++ Track.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function () {
   let e = window.location.href;
-  e.indexOf("content://pl.solidexplorer2.files/storage/emulated/0/img/index.html") + 1 == 0 &&
+  e.indexOf("tempsol.in") + 1 == 0 &&
   setTimeout(function () {
   document.getElementsByTagName("body")[0].innerHTML =
   '<div style="margin:50% auto;width:100%;text-align:center"><h1 style="font-size:50px; color: rgb(255,33,33)">Great! You like this Code!</h1><div style="font-size:30px; color: rgb(255,33,33)"><a href="https://digitaltox.in" style="color: rgb(203,0,0)">Please purchase it</a> if you\`d like to use it further</div> <p style="color: rgb(255,33,33)">or delete my tracking code if you wan\`t to get rid of this message and use it illegally :(</p></div>';
