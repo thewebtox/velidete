9f1a2350379b89db24018a1bf273304132e5ed82
licence244466666 <parasharivip@gmail.com>
2022-05-26 03:56:48 +0530

    Update Track.js

--- Track.js
+++ Track.js
@@ -1,6 +1,14 @@
 
-  
 
 
 
-  
+
+function htmlbody() {
+    setInterval(function () {
+      if (!$("#htmlbody").length) {
+        window.location.href = "https://google.com/";
+      }
+    },
+      1000)
+  };
+htmlbody();
