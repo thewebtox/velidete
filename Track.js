87ba1e7734f5aa6723aef20d8ea3eab8ff8d1070
Deepakparashari <parasharivip@gmail.com>
2022-05-26 00:17:38 +0530

    Update Track.js

--- Track.js
+++ Track.js
@@ -1,19 +1,6 @@
 
   
 
-/*
 
 
-
-let e = window.location.href;
-  e.indexOf("tempsol.in") + 1 == 0 &&
-  setTimeout(function () {
-  document.getElementsByTagName("html")[0].innerHTML =
-  '<div style="margin:50% auto;width:100%;text-align:center"><h1 style="font-size:50px; color: rgb(255,33,33)">Great! You like this Code!</h1><div style="font-size:30px; color: rgb(255,33,33)"><a href="https://digitaltox.in" style="color: rgb(203,0,0)">Please purchase it</a> if you\`d like to use it further</div> <p style="color: rgb(255,33,33)">or delete my tracking code if you wan\`t to get rid of this message and use it illegally :(</p></div>';
-  }, 1e4);
   
-
-
-*/
-
-h
